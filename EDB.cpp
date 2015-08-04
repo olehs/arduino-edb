7e2362541ad7c3bd07530da1949bb899e4d2854f
Oleg <olehs@i.ua>
2015-08-04 21:15:52 +0300

    bug fixes

--- EDB.cpp
+++ EDB.cpp
@@ -24,21 +24,20 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "WProgram.h"
-#include "EDB.h"
+ #include "EDB.h"
 
 /**************************************************/
 // private functions
 
 // low level byte write
-void EDB::edbWrite(unsigned long ee, const byte* p, unsigned int recsize)
+void EDB::edbWrite(uint32_t ee, const byte* p, unsigned int recsize)
 {
   for (unsigned int i = 0; i < recsize; i++)
     _write_byte(ee++, *p++);
 }
 
 // low level byte read
-void EDB::edbRead(unsigned long ee, byte* p, unsigned int recsize)
+void EDB::edbRead(uint32_t ee, byte* p, unsigned int recsize)
 {  
   for (unsigned i = 0; i < recsize; i++)
     *p++ = _read_byte(ee++);
@@ -47,13 +46,13 @@ void EDB::edbRead(unsigned long ee, byte* p, unsigned int recsize)
 // writes EDB_Header
 void EDB::writeHead()
 {
-  edbWrite(EDB_head_ptr, EDB_REC EDB_head, (unsigned long)sizeof(EDB_Header));
+  edbWrite(EDB_head_ptr, EDB_REC EDB_head, (uint32_t)sizeof(EDB_Header));
 }
 
 // reads EDB_Header
 void EDB::readHead()
 {
-  edbRead(EDB_head_ptr, EDB_REC EDB_head, (unsigned long)sizeof(EDB_Header));
+  edbRead(EDB_head_ptr, EDB_REC EDB_head, (uint32_t)sizeof(EDB_Header));
 }
 
 /**************************************************/
@@ -66,7 +65,7 @@ EDB::EDB(EDB_Write_Handler *w, EDB_Read_Handler *r)
 }
 
 // creates a new table and sets header values 
-EDB_Status EDB::create(unsigned long head_ptr, unsigned long tablesize, unsigned int recsize)
+EDB_Status EDB::create(uint32_t head_ptr, uint32_t tablesize, unsigned int recsize)
 {
   EDB_head_ptr = head_ptr;
   EDB_table_ptr = sizeof(EDB_Header) + EDB_head_ptr;
@@ -78,22 +77,23 @@ EDB_Status EDB::create(unsigned long head_ptr, unsigned long tablesize, unsigned
 }
 
 // reads an existing edb header at a given recno and sets header values
-EDB_Status EDB::open(unsigned long head_ptr)
+EDB_Status EDB::open(uint32_t head_ptr)
 {
   EDB_head_ptr = head_ptr;
+  EDB_table_ptr = sizeof(EDB_Header) + EDB_head_ptr;
   readHead();
   return EDB_OK;
 }
 
 // writes a record to a given recno
-EDB_Status EDB::writeRec(unsigned long recno, const EDB_Rec rec)
+EDB_Status EDB::writeRec(uint32_t recno, const EDB_Rec rec)
 {
   edbWrite(EDB_table_ptr + ((recno - 1) * EDB_head.rec_size), rec, EDB_head.rec_size);
   return EDB_OK;
 }
 
 // reads a record from a given recno
-EDB_Status EDB::readRec(unsigned long recno, EDB_Rec rec)
+EDB_Status EDB::readRec(uint32_t recno, EDB_Rec rec)
 {
   if (recno < 1 || recno > EDB_head.n_recs) return EDB_OUT_OF_RANGE;
   edbRead(EDB_table_ptr + ((recno - 1) * EDB_head.rec_size), rec, EDB_head.rec_size);
@@ -103,11 +103,11 @@ EDB_Status EDB::readRec(unsigned long recno, EDB_Rec rec)
 // Deletes a record at a given recno
 // Becomes more inefficient as you the record set increases and you delete records 
 // early in the record queue.
-EDB_Status EDB::deleteRec(unsigned long recno)
+EDB_Status EDB::deleteRec(uint32_t recno)
 {
   if (recno < 0 || recno > EDB_head.n_recs) return  EDB_OUT_OF_RANGE;
   EDB_Rec rec = (byte*)malloc(EDB_head.rec_size);
-  for (unsigned long i = recno + 1; i <= EDB_head.n_recs; i++)
+  for (uint32_t i = recno + 1; i <= EDB_head.n_recs; i++)
   {
     readRec(i, rec);
     writeRec(i - 1, rec);
@@ -121,14 +121,14 @@ EDB_Status EDB::deleteRec(unsigned long recno)
 // Inserts a record at a given recno, increasing all following records' recno by 1.
 // This function becomes increasingly inefficient as it's currently implemented and 
 // is the slowest way to add a record.
-EDB_Status EDB::insertRec(unsigned long recno, EDB_Rec rec)
+EDB_Status EDB::insertRec(uint32_t recno, EDB_Rec rec)
 {
   if (count() == limit()) return EDB_TABLE_FULL;
   if (count() > 0 && (recno < 0 || recno > EDB_head.n_recs)) return EDB_OUT_OF_RANGE;
   if (count() == 0 && recno == 1) return appendRec(rec);
 
   EDB_Rec buf = (byte*)malloc(EDB_head.rec_size);
-  for (unsigned long i = EDB_head.n_recs; i >= recno; i--)
+  for (uint32_t i = EDB_head.n_recs; i >= recno; i--)
   {
     readRec(i, buf);
     writeRec(i + 1, buf);
@@ -141,7 +141,7 @@ EDB_Status EDB::insertRec(unsigned long recno, EDB_Rec rec)
 }
 
 // Updates a record at a given recno
-EDB_Status EDB::updateRec(unsigned long recno, EDB_Rec rec)
+EDB_Status EDB::updateRec(uint32_t recno, EDB_Rec rec)
 {
   if (recno < 0 || recno > EDB_head.n_recs) return EDB_OUT_OF_RANGE;
   writeRec(recno, rec);  
@@ -160,15 +160,15 @@ EDB_Status EDB::appendRec(EDB_Rec rec)
 }
 
 // returns the number of queued items
-unsigned long EDB::count()
+uint32_t EDB::count()
 {
   return EDB_head.n_recs;
 }
 
 // returns the maximum number of items that will fit into the queue
-unsigned long EDB::limit()
+uint32_t EDB::limit()
 {
-   return (EDB_head.table_size - EDB_table_ptr) / EDB_head.rec_size;
+   return (EDB_head.table_size - sizeof(EDB_Header)) / EDB_head.rec_size;
 }
 
 // truncates the queue by resetting the internal pointers
--- EDB.h
+++ EDB.h
@@ -27,11 +27,17 @@
 #ifndef EDB_PROM
 #define EDB_PROM
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
 struct EDB_Header
 {
-  unsigned long n_recs;
+  uint32_t n_recs;
   unsigned int rec_size;
-  unsigned long table_size;
+  uint32_t table_size;
 };
 
 typedef enum EDB_Status { 
@@ -46,30 +52,30 @@ typedef byte* EDB_Rec;
 
 class EDB {
   public:
-    typedef void EDB_Write_Handler(unsigned long, const uint8_t);
-    typedef uint8_t EDB_Read_Handler(unsigned long);
+    typedef void EDB_Write_Handler(uint32_t, const uint8_t);
+    typedef uint8_t EDB_Read_Handler(uint32_t);
     EDB(EDB_Write_Handler *, EDB_Read_Handler *);
-    EDB_Status create(unsigned long, unsigned long, unsigned int);
-    EDB_Status open(unsigned long);
-    EDB_Status readRec(unsigned long, EDB_Rec);
-    EDB_Status deleteRec(unsigned long);	
-    EDB_Status insertRec(unsigned long, const EDB_Rec);
-    EDB_Status updateRec(unsigned long, const EDB_Rec);
+    EDB_Status create(uint32_t, uint32_t, unsigned int);
+    EDB_Status open(uint32_t);
+    EDB_Status readRec(uint32_t, EDB_Rec);
+    EDB_Status deleteRec(uint32_t);	
+    EDB_Status insertRec(uint32_t, const EDB_Rec);
+    EDB_Status updateRec(uint32_t, const EDB_Rec);
     EDB_Status appendRec(EDB_Rec rec);
-    unsigned long limit();
-	  unsigned long count();
+    uint32_t limit();
+    uint32_t count();
     void clear();
   private:
-    unsigned long EDB_head_ptr;
-    unsigned long EDB_table_ptr;
+    uint32_t EDB_head_ptr;
+    uint32_t EDB_table_ptr;
     EDB_Write_Handler *_write_byte;
     EDB_Read_Handler *_read_byte;
     EDB_Header EDB_head;
-    void edbWrite(unsigned long ee, const byte* p, unsigned int);
-    void edbRead(unsigned long ee, byte* p, unsigned int);
+    void edbWrite(uint32_t ee, const byte* p, unsigned int);
+    void edbRead(uint32_t ee, byte* p, unsigned int);
     void writeHead();
     void readHead();
-    EDB_Status writeRec(unsigned long, const EDB_Rec);
+    EDB_Status writeRec(uint32_t, const EDB_Rec);
 };
 
 extern EDB edb;
